bdb3802d034a9e94b9c5d7f4bf7f34b2eab859ac
Niovi Megiris <niovimegiris@gmail.com>
2018-01-05 00:30:29 +0200

    Resolved boolean issue

--- File.py
+++ File.py
@@ -2,6 +2,6 @@ budget = int(input("What is your budget? "))
 dates_to_explore = input("When are you traveling?" )
 time_allocation = input("Start Location: ")
 dest_neighborhood = input("Which neighborhood in NYC would you like to visit? (ex. Upper West Side, Soho, etc.) ")
-end_location = boolean(input("Would you like to return to your start location? "))
+end_location = bool(input("Would you like to return to your start location? "))
 
 # hi this is an edit
