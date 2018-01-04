4a65a7477125d822e39a4e3234772c1ec2037c72
Niovi Megiris <niovimegiris@gmail.com>
2018-01-05 00:25:12 +0200

    Fixed input commands

--- File.py
+++ File.py
@@ -1,7 +1,7 @@
-budget = raw_input("What is your budget? ")
-dates_to_explore = raw_input("When are you traveling?" )
-time_allocation = raw_input("Start Location: ")
-dest_neighborhood = raw_input("Which neighborhood in NYC would you like to visit? (ex. Upper West Side, Soho, etc.) ")
-end_location = boolean(raw_input("Would you like to return to your start location? "))
+budget = int(input("What is your budget? "))
+dates_to_explore = input("When are you traveling?" )
+time_allocation = input("Start Location: ")
+dest_neighborhood = input("Which neighborhood in NYC would you like to visit? (ex. Upper West Side, Soho, etc.) ")
+end_location = boolean(input("Would you like to return to your start location? "))
 
 # hi this is an edit
