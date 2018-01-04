238cab771243751e857a3f6110e99c686bdf0015
niovimegiris <34302852+niovimegiris@users.noreply.github.com>
2018-01-04 23:28:42 +0200

    Add files via upload

---
+++ File.py
@@ -0,0 +1,5 @@
+budget = raw_input("What is your budget? ")
+dates_to_explore = raw_input("When are you traveling?" )
+time_allocation = raw_input("Start Location: ")
+dest_neighborhood = raw_input("Which neighborhood in NYC would you like to visit? (ex. Upper West Side, Soho, etc.) ")
+end_location = boolean(raw_input("Would you like to return to your start location? "))
