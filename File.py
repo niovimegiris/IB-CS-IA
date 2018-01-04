9d8662572b1297393c116a0b22c9b6743fd8aac7
Niovi Megiris <niovimegiris@gmail.com>
2018-01-05 00:16:18 +0200

    adding a comment

--- File.py
+++ File.py
@@ -3,3 +3,5 @@ dates_to_explore = raw_input("When are you traveling?" )
 time_allocation = raw_input("Start Location: ")
 dest_neighborhood = raw_input("Which neighborhood in NYC would you like to visit? (ex. Upper West Side, Soho, etc.) ")
 end_location = boolean(raw_input("Would you like to return to your start location? "))
+
+# hi this is an edit
