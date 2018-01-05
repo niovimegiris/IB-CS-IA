152a8c877491a6202b8f922f8a08a35768073dd8
Niovi Megiris <niovimegiris@gmail.com>
2018-01-06 01:45:10 +0200

    Adding user input exceptions

--- File.py
+++ File.py
@@ -1,7 +1,32 @@
-budget = int(input("What is your budget? "))
-dates_to_explore = input("When are you traveling?" )
-time_allocation = input("Start Location: ")
+##def checkIntType(input_variable):
+##    if input_variable == int:
+##        return input_variable
+##    else:
+##        return ("I didn't understand that")
+
+
+# User input
+#def gatherInputs():
+while True:
+    try:
+        budget = int(input("What is your budget? "))
+    except ValueError:
+        print("Sorry I didn't understand that") # return to start of loop
+        continue
+    else:
+        break # success
+    
+while True:    
+    day_to_explore = input("When are you traveling?" )
+    if not day_to_explore.isalpha():
+        print("Sorry I didn't get that")
+        continue
+    else:
+        break
+
+
+start_location = input("Start Location: ")
 dest_neighborhood = input("Which neighborhood in NYC would you like to visit? (ex. Upper West Side, Soho, etc.) ")
 end_location = bool(input("Would you like to return to your start location? "))
 
-# hi this is an edit
+
