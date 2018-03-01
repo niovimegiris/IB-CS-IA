f2153a14af2ba944bf8890be73b09d9035b61361
Niovi Megiris <niovimegiris@gmail.com>
2018-03-01 22:43:37 +0200

    Adding to API Code

--- API_HelperFunctions.py
+++ API_HelperFunctions.py
@@ -3,8 +3,13 @@
 import json
 import requests
 
+#API Keys
+google_maps_key = 'AIzaSyAWNzuFCqmmZQwRyg5vmOwnLDfv0Ma0o5s'
+foursquare_id = 'KO2FJZFUOY4SF0JEBVXA4DYVHY4QF4IJEC1IHB2XORULPSVE'
+foursquare_secret = 'IGZJ0YRZVTMSSZ0C2T0Z4YRUN2U0AGGZTRP5JAGFQU5TMIIF'
+
 # FOURSQUARE
-def getTopVenues(location, section = 'topPicks', num_locations = 10):
+def getTopVenues(client_id, client_secret, location, section = 'topPicks', num_locations = 50, offset = 20):
     """
     INPUT:
     - location: string containing location
@@ -15,20 +20,35 @@ def getTopVenues(location, section = 'topPicks', num_locations = 10):
     """
     url = 'https://api.foursquare.com/v2/venues/explore'
     params = dict(
-        client_id = 'KO2FJZFUOY4SF0JEBVXA4DYVHY4QF4IJEC1IHB2XORULPSVE',
-        client_secret = 'IGZJ0YRZVTMSSZ0C2T0Z4YRUN2U0AGGZTRP5JAGFQU5TMIIF',
+        client_id = client_id,
+        client_secret = client_secret,
         v = '20170001',
-        near = num_locations,
+        near = location,
         section = section,
-        limit = 10
+        limit = num_locations,
+        offset = offset # pages through results (which are random)
         )
     #print ("Searching the web for some things to do in {}!").format(location)
     resp = requests.get(url=url, params=params)
     data = json.loads(resp.text)
-    return data
+    #return data #returns all data, however we want to return a subset
 
-test = getTopVenues('Soho, NY')
+    #Retrieve both the names of the top venues
+    top_venues_list = [] #this is a list, not a dictionary
+    for each_location in range(0, num_locations): #loops through each iteration
+        venue_name = data['response']['groups'][0]['items'][each_location]['venue']['name'] # Subset of data
+        top_venues_list.append(venue_name) # Adds venue (element) to existing list of venues
+    return top_venues_list
+
+#ISSUE
+    # how do we randomize the names of venues for new trips?
+
+test = getTopVenues(foursquare_id, foursquare_secret, 'Soho, NY')
 print(test)
+
+
+
+
 
 #test['meta'] everytime i request, gives unique id
 # write a for loops that loops through test['response']['groups'][0]['items'][0]['venue']['name']
@@ -43,7 +63,7 @@ print(test)
 # GOOGLE MAPS
 # key: AIzaSyAWNzuFCqmmZQwRyg5vmOwnLDfv0Ma0o5s
 
-def getRoute(start_address, end_address): #FIX GOOGLE MAPS APIIIII
+def getRoute(API_Key, start_address, end_address): #FIX GOOGLE MAPS APIIIII
     """
     INPUT:
     - start_address : starting location
@@ -55,7 +75,7 @@ def getRoute(start_address, end_address): #FIX GOOGLE MAPS APIIIII
     params = dict(
         origin= start_address,
         destination= end_address,
-        key='AIzaSyAWNzuFCqmmZQwRyg5vmOwnLDfv0Ma0o5s'
+        key= API_Key
         )
 
     #print ("Searching Google Maps for best route to take from {0} to {1}").format(start_address, end_address)
@@ -63,5 +83,6 @@ def getRoute(start_address, end_address): #FIX GOOGLE MAPS APIIIII
     data = json.loads(resp.text)
     return data
 
-test = getRoute('Soho', 'Brooklyn')
-print(test)
+test = getRoute(google_maps_key, 'Soho', 'Brooklyn')
+
+#print(test) #testing
