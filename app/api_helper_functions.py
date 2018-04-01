720d09e004ecf9c22e2a314c06183d0c70cfad0e
Niovi Megiris <niovimegiris@gmail.com>
2018-04-01 22:26:56 +0300

    Optimized Route and added to Form that Outputs website

--- app/api_helper_functions.py
+++ app/api_helper_functions.py
@@ -35,7 +35,7 @@ def getTopVenues(client_id, client_secret, location, section = 'topPicks', num_l
     resp = requests.get(url=url, params=params)
     data = json.loads(resp.text)
     #return data #returns all data, however we want to return a subset
-    
+
        #Retrieve both the names of the top venues
     top_venues_list = [] #this is a list, not a dictionary
     for each_location in data['response']['groups'][0]['items']: #loops through each iteration
@@ -56,7 +56,7 @@ venues = randomlySelectVenues(venues, 5) # will randomly select 5 venues
 
 #Issue #2
     # how do we make sure the selected venues are without replacement (dont repeat)?
-    #ANSWER: USE NUMPY LIBRARY 
+    #ANSWER: USE NUMPY LIBRARY
 print(venues)
 
 
@@ -87,7 +87,7 @@ def getRoute(api_key, start_address, end_address, venues): #FIX GOOGLE MAPS APII
     for venue in venues:
         waypoints = waypoints + '|{0}'.format(venue) # pipe | separates the venues in the list
     # play around with test
-    
+
     params = dict(
         origin=start_address,
         destination=end_address,
@@ -98,7 +98,7 @@ def getRoute(api_key, start_address, end_address, venues): #FIX GOOGLE MAPS APII
     #print ("Searching Google Maps for best route to take from {0} to {1}").format(start_address, end_address)
     resp = requests.get(url=url, params=params)
     data = json.loads(resp.text)
-    
+
     durations_list = [] # list to store durations of trips (strings)
     durations_list_values = [] # list to store durations of trips (as values/integers)
     for each_leg in data['routes'][0]['legs']:
@@ -113,17 +113,31 @@ travel_times, travel_times_values = getRoute(google_maps_key, 'Soho', 'Brooklyn'
 
 print(travel_times, travel_times_values) #testing
 
+while abs(trip_time_diff) <= last_trip_time_diff:
+    optimized_route = get_route(origin, destination, venues_selected, travel_mode, return_to_start)
+    trip_time_diff = (trip_duration*60) - (sum(leg['trip_duration_value'] for leg in optimized_route)/60 + len(venues_selected) * avg_time)
+    if abs(trip_time_diff) <= last_trip_time_diff:
+        last_trip_time_diff = abs(trip_time_diff)
+        previous_route = optimized_route # replaces old route with new router
+        if trip_time_diff > 0:
+            # adds one venue from route (randomly)
+            venues_selected.append(random.sample(venues, 1))
+        else:
+            # removes one venue from the router
+            venues_selected.pop()
+return previous_route
+
 def buildTripPlan(venues, travel_times, travel_times_values, back_to_origin = True, avg_time_per_venue = 30):
     trip_plan = '' #default
 
     trip_duration = len(venues) * avg_time_per_venue + math.floor(sum(travel_times_values) / 60)
-    trip_duration_hours = math.floor(trip_duration / 60) 
-    trip_duration_minutes = trip_duration % 60 
+    trip_duration_hours = math.floor(trip_duration / 60)
+    trip_duration_minutes = trip_duration % 60
     if trip_duration_minutes == 0:
         trip_plan = trip_plan + '\n Your trip will take {0} hours'.format(trip_duration_hours)
-    else: 
+    else:
         trip_plan = trip_plan + '\n Your trip will take {0} hours and {1} minutes'.format(trip_duration_hours, trip_duration_minutes)
-        
+
     for step_number, venue in enumerate(venues):
         trip_plan = trip_plan + '\n Step {0}: Travel {1} to {2}'.format(step_number + 1, travel_times[step_number], venue)
     if back_to_origin:
